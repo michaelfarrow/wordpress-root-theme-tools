c2cd961c170d0416b52bee86a1d1c60f660f7343
Mike Farrow <contact@mikefarrow.co.uk>
2015-08-01 17:26:30 +0100

    fixing git bug

--- includes/git.php
+++ includes/git.php
@@ -1,21 +1,35 @@
 <?php
 
+  function roots_current_git_head(){
+    $head = @file(ABSPATH . '../../../.git/HEAD' );
+
+    if(!$head) return '';
+
+    return $head[0];
+  }
+
   function root_current_git_commit($branch = null) {
     if(!$branch) $branch = root_current_git_branch();
 
-     if ( $hash = @file(ABSPATH . '../../../.git/refs/heads/' . $branch)) {
+     if($branch === false){
+       $hash = roots_current_git_head();
+     }else{
+       $hash = @file(ABSPATH . '../../../.git/refs/heads/' . $branch);
+       if($hash) $hash = $hash[0];
+     }
 
-      return substr($hash[0], 0, 7);
+     if ( $hash ) {
+      return substr($hash, 0, 7);
     }
   }
 
   function root_current_git_branch() {
-    $stringfromfile = @file(ABSPATH . '../../../.git/HEAD' );
+    $head = roots_current_git_head();
 
-    if(!$stringfromfile) return '';
+    $explodedstring = explode("/", $head, 3);
 
-    $firstLine = $stringfromfile[0];
-    $explodedstring = explode("/", $firstLine, 3);
+    if(count($explodedstring) != 3) return false;
+
     $branchname = $explodedstring[2];
 
     return trim($branchname);
